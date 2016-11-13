9e19b6e93d08df102678d51b9ea76040ae061ac1
MohameddOsamaa <mohamedd.osamaa@gmail.com>
2016-11-13 17:31:25 +0200

    Add files via upload

--- Assignment3/app.js
+++ Assignment3/app.js
@@ -5,59 +5,50 @@ angular.module('NarrowItDownApp',[])
 .controller('NarrowItDownController',NarrowItDownController) //will wrap your search textbox and button as well as the list of found items.
 .service('MenuSearchService',MenuSearchService)
 .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
-.directive('foundItems', foundItems);
+.directive('foundItems', FoundItems);
 
-function foundItems() {
+function FoundItems() {
   var ddo = {
-    templateUrl: 'itemsloaderindicator.html',
+    templateUrl: 'itemsloader.html',
     scope: {
-      founditem: '<',
+      items: '<',
       onRemove: '&'
-    },
-    controller: foundItemsDirectiveController,
-    controllerAs: 'foundItem',
-    bindToController: true
+    }
   };
 
   return ddo;
 }
 
 
-function foundItemsDirectiveController() {
-  var foundItemctrl = this;
-  
-}
-
 
 NarrowItDownController.$inject=['MenuSearchService'];
 function NarrowItDownController(MenuSearchService){
-	var NIDCtrl = this;
-  NIDCtrl.itemName = "";
-  NIDCtrl.show = "";
-  NIDCtrl.foundItems = [];
+	var list = this;
+  list.itemName = "";
+  list.items = [];
+  
 
-	NIDCtrl.matchitem = function(itemName){
-    if(!NIDCtrl.itemName){
-      NIDCtrl.show = "Nothing found";
+
+	list.matchitem = function(){
+    list.items = [];
+    if(!list.itemName){
+      list.show = "Nothing found";
       return;
     }
-		var promise = MenuSearchService.getMatchedMenuItems();
+		var promise = MenuSearchService.getMatchedMenuItems(list.itemName);
 
-
-    promise.then(function (response) {
-      var menuItemList = response.data.menu_items;
-      if(searchTerm){
-        for(var i=0 ; i < menuItemList.length ; i++){
-          if(menuItemList[i].descreption.indexOf(searchItem.toLowerCase()) !== -1){
-            NIDCtrl.foundItems.push(menuItemList[i]);
-          }
-        }
-      }
+    promise.then(function(items) {
+      list.items = items;
+   
+      list.show = "";
     })
-	};
+    .catch(function(error) {
+      list.show = "Nothing found";
+    });
+	}
 
-  NIDCtrl.removeItem = function (itemIndex) {
-    foundItems.removeItem(itemIndex);
+  list.removeItem = function (itemIndex) {
+    list.items.splice(itemIndex,1);
   };
 
 }
@@ -70,39 +61,38 @@ function MenuSearchService($http, ApiBasePath){
 
   
 
-	service.getMatchedMenuItems = function(){
-	    var response =  $http({
+	service.getMatchedMenuItems = function(itemName){
+    var service = this;
+    var foundItems = [];
+	    return  $http({
         method: "GET",
         url: (ApiBasePath + "/menu_items.json"),
-        params: {category: searchTerm}
+      }).then(function (response) {
+        var show = "";
+          var menuItemList = response.data.menu_items;
+          var foundItems = [];
+          for(var i=0; i < menuItemList.length; i++){
+
+            if(menuItemList[i].description.indexOf(itemName) !== -1){
+
+              foundItems.push(menuItemList[i]);
+
+              console.log("matched   i=" + i);
+            }
+          };
+          return foundItems;
+
+          if (foundItems.length <= 0) {
+            show = "Nothing found";
+          }
       });
+  };
 
-      return response;
-
-	};
 
   service.removeitem = function (itemIndex) {
-
-    found.splice(Index,1);
-
+    foundItems.splice(itemIndex,1);
   };
 
 }
 
 })();
-
-
-// .then (function(response){
-//           var menuItemList = response.data.menu_items;
-
-//           var foundItems = [];
-//           if(searchTerm){
-//             for(var i=0 ; i < menuItemList.length ; i++){
-//               if(menuItemList[i].descreption.indexOf(searchItem.toLowerCase()) !== -1){
-//                 foundItems.push(menuItemList[i]);
-//               }
-//             }
-//           }
-
-//           return foundItems;
-//       })
